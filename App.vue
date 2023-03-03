9aeacd02486f251ee7a813f849dbef61ac306893
Deng-jiaqi <55381840+Deng-jiaqi@users.noreply.github.com>
2023-03-03 15:30:53 +0800

    add 备份

---
+++ App.vue
@@ -0,0 +1,56 @@
+<template>
+	<!--  v-if="$route.path" -->
+	<view id="app">
+		<keep-alive>
+			<router-view></router-view>
+		</keep-alive>
+		<!-- <alarm-clock ref="alarmClockRefs"></alarm-clock> -->
+	</view>
+</template>
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch');
+		},
+		onShow: function() {
+			console.log(uni.getStorageSync("token"))
+		},
+		onHide: function() {
+			console.log('App Hide')
+		},
+		globalData: {
+			forum_id: ''
+		}
+	}
+</script>
+
+<style lang="scss">
+	//隐藏滚动条
+	// ::-webkit-scrollbar {
+	// 	display: none;
+	// 	width: 0 !important;
+	// 	height: 0 !important;
+	// 	-webkit-appearance: none;
+	// 	background: transparent;
+	// }
+
+	/*每个页面公共css */
+	@import "uview-ui/index.scss";
+
+	* {
+		-webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+		-webkit-tap-highlight-color: transparent;
+	}
+
+	a,
+	input,
+	button,
+	li {
+
+		-webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+
+		-webkit-tap-highlight-color: transparent;
+		/*特殊的安卓4.0版本一下也会存在高亮效果，利用这点可做到消除*/
+
+	}
+</style>
---
+++ main.js
@@ -0,0 +1,66 @@
+import App from './App'
+
+// #ifndef VUE3
+import Vue from 'vue'
+// main.js，注意要在use方法之后执行
+import uView from 'uview-ui'
+// import Echarts from 'echarts'
+import alarmClock from '@/components/alarm-clock/index.vue'
+import upperTable from '@/components/upper-table/index.vue'
+import footNav from '@/components/foot-nav/foot-nav.vue'
+import chartCurve from '@/components/canvas-curve/index.vue'
+import canvasGame from '@/components/canvas-game.vue'
+import store from './store'
+// 小程序分享
+import share from 'static/share.js'
+
+// import Vconsole from 'vconsole';
+Vue.prototype.$store = store
+// Vue.prototype.echarts = Echarts
+Vue.mixin(share)
+Vue.use(uView)
+//点击音效
+Vue.prototype.ScanAudio = function() {
+	var music = null;
+	music = uni.createInnerAudioContext(); //创建播放器对象
+	music.src = "/package3/static/assets/speed/yinxiao.mp3";
+	music.play(); //执行执行播放
+	music.onEnded(() => {
+		//音频播放结束
+		music = null;
+
+	});
+}
+// Vue.use(Echarts)
+// let vConsole = new Vconsole();
+// 如此配置即可
+// uni.$u.config.unit = 'rpx'
+Vue.prototype.naozhongKey = false
+Vue.prototype.naozhongTimer = null
+Vue.prototype.globalVideoCover = ''
+Vue.config.productionTip = false
+Vue.component('foot-nav', footNav)
+Vue.component('alarm-clock', alarmClock);
+Vue.component('upper-table', upperTable);
+Vue.component("canvas-curve", chartCurve);
+Vue.component("canvas-game", canvasGame);
+
+App.mpType = 'app'
+const app = new Vue({
+	...App
+})
+app.$mount()
+// #endif
+
+// #ifdef VUE3
+import {
+	createSSRApp
+} from 'vue'
+
+export function createApp() {
+	const app = createSSRApp(App)
+	return {
+		app
+	}
+}
+// #endif
